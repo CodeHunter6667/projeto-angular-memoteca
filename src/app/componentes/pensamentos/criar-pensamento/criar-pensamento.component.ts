126e7274ad14e730800ad6660332eff585019aa9
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-02-28 16:31:48 -0300

    feat: integracao com backend para criar pensamento

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -7,22 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento = {
-    id: '1',
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
     modelo: ''
   }
 
   criarPensamento(){
-    alert("Pensamento foi criado");
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
   }
 
   cancelaPensamento(){
     alert("Pensamento foi cancelado");
   }
 
-  constructor() { }
+  constructor(private service: PensamentoService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
